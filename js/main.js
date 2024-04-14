4b67eb4008f9d705363ae794648bf2c001a824be
lucia <lucia.alfano07@gmail.com>
2024-04-14 16:55:11 -0300

    preentrega 3

--- js/main.js
+++ js/main.js
@@ -1,43 +1,64 @@
-let edad = prompt("Ingrese su edad");
+const loginForm = document.getElementById("login-form");
+const mensajeError = document.getElementById("mensaje-error");
+
+loginForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "Lucia" && password === "12345678") {
+        
+        alert("Bienvenido!");
+       
+    } else {
+       
+        mensajeError.innerText = "Nombre de usuario o contraseña incorrectos";
+    }
+});
+
+// let edad = prompt("Ingrese su edad");
 
 // Validación de la edad
-if (edad >= 18) {
-    const persona = {
-        nombre: prompt("Por favor, ingrese su nombre"),
-        localidad: prompt("Por favor, ingrese su localidad"),
+// if (edad >= 18) {
+//     const persona = {
+//         nombre: prompt("Por favor, ingrese su nombre"),
+//         localidad: prdocument.addEventListener("DOMContentLoaded", function() {
+  
+// });ompt("Por favor, ingrese su localidad"),
 
-        verInfo: function() {
-            console.log("Nombre: " + this.nombre, + " " + "Localidad: " + this.localidad);
-        }
-    };
+//         verInfo: function() {
+//             console.log("Nombre: " + this.nombre, + " " + "Localidad: " + this.localidad);
+//         }
+//     };
 
-    alert("¡Bienvenido!");
-    persona.verInfo(); 
-} else {
-    do {
-        alert("Usted no posee permisos para ingresar");
-        console.log("Menor de edad");
-        edad = prompt("Ingrese su edad"); // Solicita nuevamente la edad hasta que sea mayor o igual a 18
-    } while (edad < 18)
-}
+//     alert("¡Bienvenido!");
+//     persona.verInfo(); 
+// } else {
+//     do {
+//         alert("Usted no posee permisos para ingresar");
+//         console.log("Menor de edad");
+//         edad = prompt("Ingrese su edad"); // Solicita nuevamente la edad hasta que sea mayor o igual a 18
+//     } while (edad < 18)
+// }
 
-let ingresoMensual = Number(prompt("Por favor, indique su ingreso mensual neto"));
-if (ingresoMensual >= 450000) {
-    alert("Usted tiene una cuenta Platinum!");
-    console.log("Usuario válido PLATINUM");
-} else if (ingresoMensual >= 300000) {
-    alert("Usted tiene una cuenta Gold!");
-    console.log("Usuario válido Gold");
-} else {
-    alert("Usted posee una cuenta básica");
-    console.log("Usuario válido BÁSICO");
-}
+// let ingresoMensual = Number(prompt("Por favor, indique su ingreso mensual neto"));
+// if (ingresoMensual >= 450000) {
+//     alert("Usted tiene una cuenta Platinum!");
+//     console.log("Usuario válido PLATINUM");
+// } else if (ingresoMensual >= 300000) {
+//     alert("Usted tiene una cuenta Gold!");
+//     console.log("Usuario válido Gold");
+// } else {
+//     alert("Usted posee una cuenta básica");
+//     console.log("Usuario válido BÁSICO");
+// }
 
 //array 1
 const beneficiosGold = ["tarjeta de credito VISA" , "tarjeta de credito Mastercard" , "Prestamo"]
 const beneficiosPlatinum = ["Beneficios GOLD" , "Cobertura Internacional" , "Limite flexible"]
 const beneficiosBlack = beneficiosGold.concat(beneficiosPlatinum)
-console.log(beneficiosBlack);
+// console.log(beneficiosBlack);
 
 //map
 
@@ -51,18 +72,3 @@ const tipoDeCuentaCredito = [
  const nombres = tipoDeCuentaCredito.map((el) => el.nombre)
  console.log(nombres)
 
- const precios = tipoDeCuentaCredito.map((el) => {
-    return {
-        nombre: el.nombre,
-        limite: el.limite*1.20
-    }
- })
-
- console.log(precios);
-
-//valor singular
-const hoy = new Date  ("March 20, 2024");
-
-console.log(hoy.getFullYear())
-console.log(hoy.getMonth())
-console.log(hoy.getDate())
